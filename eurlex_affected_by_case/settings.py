97b8ffa4c05520ee1e187d3a49f3e9427be60e33
Espen Evensen <espen@evensen.io>
2021-01-17 20:18:19 +0100

    added Requests

--- eurlex_affected_by_case/settings.py
+++ eurlex_affected_by_case/settings.py
@@ -30,7 +30,7 @@ FEED_EXPORT_FIELDS = [
 # USER_AGENT = 'eurlex_affected_by_case (+https://evensen.io)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 3
@@ -44,16 +44,21 @@ ROBOTSTXT_OBEY = True
 # CONCURRENT_REQUESTS_PER_IP = 3
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#     'Accept-Language': 'en',
-# }
+DEFAULT_REQUEST_HEADERS = {
+    ':authority: eur-lex.europa.eu': 'eur-lex.europa.eu',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': 'ELX_SESSIONID=5SoMvVCsIXLVOFLKqRdcsecR1h0RqoaXuLDY7i2Bm0DNCEzcDZqo!-643854698; ppms_privacy_0b5594a8-b9c8-4cd6-aa25-5c578dcf91df={%22consents%22:{%22analytics%22:{%22status%22:-1%2C%22updatedAt%22:%222021-01-16T19:47:53.415Z%22}}%2C%22domain%22:{%22normalized%22:%22eur-lex.europa.eu%22%2C%22isWildcard%22:false%2C%22pattern%22:%22eur-lex.europa.eu%22}}; _pk_ses.61.5596=*; _pk_ses.0b5594a8-b9c8-4cd6-aa25-5c578dcf91df.5596=*; _pk_id.61.5596=f4cc1f89bb152f41.1610826476.1.1610826573.1610826476.; WT_FPC=id=192.168.13.1-64399600.30862400:lv=1610801373601:ss=1610801277815; _pk_id.0b5594a8-b9c8-4cd6-aa25-5c578dcf91df.5596=32916e5ea3cb7cbb.1610826476.1.1610826574.1610826476.; AWSALB=skpn/9z2HNuBSnGrXxPeVSiYBZ12QF3HVAYleIqM+Z62yip/lemPJhT3oxLiaVeGO38rlUp1IhZ/z1gB7CNJP/7D9IZK4RQAgri7E9Ha7pYVLpk+EG6sqJHWvMN4',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+    'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+    'referrer': 'eur-lex.europa.eu'
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -63,11 +68,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
     # 'eurlex_affected_by_case.middlewares.EurlexAffectedByCaseDownloaderMiddleware': 543,
-# }
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -83,14 +88,14 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = False
 
@@ -99,5 +104,12 @@ AUTOTHROTTLE_DEBUG = False
 # HTTPCACHE_ENABLED = True
 # HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_IGNORE_HTTP_CODES = [404]
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+
+# handle_httpstatus_list = [404]
+RETRY_ENABLED = True
+RETRY_TIMES = 5
+RETRY_HTTP_CODES = [500, 503, 504, 400, 404, 408]
--- eurlex_affected_by_case/spiders/affected_by_case.py
+++ eurlex_affected_by_case/spiders/affected_by_case.py
@@ -1,15 +1,18 @@
 import scrapy
 import pandas as pd
+from scrapy import Selector
+import requests
 
 
 class AffectedByCaseSpider(scrapy.Spider):
     name = 'affected_by_case'
-    allowed_domains = ['eur-lex.europa.eu']
+    handle_httpstatus_list = [404, 500]
+    allowed_domains = []
 
     def start_requests(self):
         csv_file = "./csv/celex-numbers.csv"
         df = pd.read_csv(csv_file, usecols=["acq_recno", "celex_number",
-                                                       "case_status"])  # extract three fields/columns from csv file
+                                            "case_status"])  # extract three fields/columns from csv file
         df = df.drop(df[df.celex_number.isnull()].index)  # drop row if field/column 'celex_number' is null
         df = df.drop(
             df[df.celex_number.str[0] != '3'].index)  # drop row if field/column 'celex_number' doesn't start with '3'
@@ -18,23 +21,53 @@ class AffectedByCaseSpider(scrapy.Spider):
         df = df.drop(df[df.case_status == 6].index)  # drop row if field/column 'case_status' is '6'
         df = df.drop(df[df.case_status == 8].index)  # drop row if field/column 'case_status' is '8'
 
-        base_url = "https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:"  # base url to be concatenated with the celex number. For example 'https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32000L0060'
+        base_url = "https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:"  # base url to be concatenated with the celex number.
 
         for row in df.iterrows():  # iterate over rows and parse generated urls based on celex numbers
             acq_recno = row[1][0]  # extract field acq_recno from csv row
             celex_number = row[1][1]  # extract field celex number from csv row
-            url = str(base_url) + str(celex_number)  # concatenate base url with celex number. For example 'https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32000L0060'
+            url = str(base_url) + str(
+                celex_number)  # concatenate base url with celex number. For example 'https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32000L0060'
             yield scrapy.Request(url, dont_filter=True, callback=self.parse,
                                  meta={'celex_number': celex_number,
                                        'acq_recno': acq_recno,
                                        })
 
     def parse(self, response):
-        celex_number = response.request.meta['celex_number']  # celex number, ex. 32000L0060
-        acq_recno = response.request.meta['acq_recno']  # unique ID from EFTA 360, ex. 318588
+        meta = response.request.meta
 
-        pplinked_affected_by_lis = response.xpath(
+        if response.status != 404:
+            self.parse_body(response.body, response.url, response.request.meta)
+        else:
+            print("\n" * 2)
+            print('URL: %s' % response.url)
+            print('SPIDER RETURNS 404 - TRYING ALTERNATIVE REQUEST METHOD...')
+            print("\n" * 2)
+
+            meta['notify'] = 1
+            x = requests.get(response.url)
+            print('STATUS: %s' % x.status_code)
+            self.parse_body(x.content, response.url, meta)
+
+    def parse_body(self, body, url, meta):
+        sel = Selector(text=body)
+
+        notfound = 'The requested document does not exist.'
+
+        if 'notify' in meta and b'The requested document does not exist.' in body:
+            print("\n" * 2)
+            print(notfound)
+            print(url)
+            print("\n" * 2)
+            return
+
+        pplinked_affected_by_lis = sel.xpath(
             "//div[@id='PPLinked_Contents']/div/dl/dt[contains(.,'Affected by case')]/following-sibling::dd[1]/ul/li")  # xpath to extract li elements under "Affected by case"
+
+        if len(pplinked_affected_by_lis) is 0:
+            # print ('URL: %s' % url)
+            # print ('Affected by case not found on document')
+            return
 
         for li in pplinked_affected_by_lis:
             affected_text = li.xpath(
@@ -44,8 +77,18 @@ class AffectedByCaseSpider(scrapy.Spider):
                 ".//a/text()").get()
 
             yield {
-                'affected_acq_recno': acq_recno,
-                'affected_celex_number': celex_number,
+                'affected_acq_recno': meta['acq_recno'],
+                'affected_celex_number': meta['celex_number'],
                 'affected_text': affected_text.strip(),
                 'affected_court_celex': affected_court_celex,
             }
+
+            if 'notify' in meta:
+                print("\n" * 2)
+                print('!!!FOUND!!!')
+                yield {
+                    'affected_acq_recno': meta['acq_recno'],
+                    'affected_celex_number': meta['celex_number'],
+                    'affected_text': affected_text.strip(),
+                    'affected_court_celex': affected_court_celex,
+                }
